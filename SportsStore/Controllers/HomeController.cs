887c9d041300c2a117898ce3a2a2f2301eac4f1a
Galkin Aleksey <galkin_aleksey@inbox.ru>
2023-12-27 23:53:35 +0300

    first commit for start working

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System.Linq;
+
+namespace SportsStore.Controllers {
+    public class HomeController : Controller {
+        private IStoreRepository repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo) {
+            repository = repo;
+        }
+
+        public ViewResult Index(int productPage = 1) =>
+            View(new ProductsListViewModel {
+                    Products = repository.Products
+                        .OrderBy(p => p.ProductID)
+                        .Skip((productPage - 1) * PageSize)
+                        .Take(PageSize), 
+                    PagingInfo = new PagingInfo {
+                        TotalItems = repository.Products.Count(), 
+                        ItemsPerPage = PageSize, 
+                        CurrentPage = productPage
+                    }
+                });
+    }
+}
